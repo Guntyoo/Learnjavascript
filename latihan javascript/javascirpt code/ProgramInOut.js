2ff2138c20ebc7fd5ac4c5a5c01cc2c355da29a1
Guntyoo <aprastiyo065@gmail.com>
2024-11-25 10:52:14 +0700

    update2 program puskesmas menambah pasien ke kursi kosong

---
+++ latihan javascript/javascirpt code/ProgramInOut.js
@@ -0,0 +1,34 @@
+//membuat program resposive angkot
+var customer = ["agung", undefined, "tyo"];
+var tambahcustomer = function (namacustomer, customer) {
+  //jika angkot terisi
+  if (customer.length == 0) {
+    // add customer
+    customer.push(namacustomer);
+    //return customer
+    return customer;
+  } else {
+    //menulusuri seluruh kursi dari kosong menggunakann looping
+    for (var i = 0; i < customer.length; i++) {
+      //jika ada kursi yang kosong
+      if (customer[i] == undefined) {
+        //menambahkan customer ke kursi
+        customer[i] = namacustomer;
+        //return value array& out at function
+        return customer;
+      }
+      //jika sudah ada nama customer yang sama
+      //maka tampilkan kesalahan
+      //return value & keluar if
+
+      //jika ada kursi kosong
+      //jika sudah ditelusuri sampai akhir tetapi ada kursi kosong dan kursi terakhir sudah terisi
+      else if (i == customer.length - 1) {
+        //tambah customer dikursi kosong
+        customer.push(namacustomer);
+        //return value dan keluar  if
+        return customer;
+      }
+    }
+  }
+};
