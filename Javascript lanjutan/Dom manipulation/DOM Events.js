80a254eeeb44fb953fc78b26aa19e58d3e5a3fa0
Guntyoo <aprastiyo065@gmail.com>
2025-01-16 21:48:33 +0700

    tired

---
+++ Javascript lanjutan/Dom manipulation/DOM Events.js
@@ -0,0 +1,10 @@
+//Events secara definisi yaitu Event diJavascript yang mempresentasikan sebuah kejadian yang terjadi pada DOM
+//event bisa dilakukan oleh user cth(menklik mouse,keyboard,dll) atau dilakukan secara otomatis oleh API
+
+//addEventListener
+const p3 = document.querySelector('.p3');
+
+function ubahWarna () {
+    p3.style.background
+}
+p3.onClick = ubahwarna;
