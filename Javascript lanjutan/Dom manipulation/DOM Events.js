abc8251ba5223ec4f990435cb19048642e3a5c20
Guntyoo <aprastiyo065@gmail.com>
2025-01-21 18:20:24 +0700

    belajar addEventListener

--- Javascript lanjutan/Dom manipulation/DOM Events.js
+++ Javascript lanjutan/Dom manipulation/DOM Events.js
@@ -1,10 +1,13 @@
 //Events secara definisi yaitu Event diJavascript yang mempresentasikan sebuah kejadian yang terjadi pada DOM
 //event bisa dilakukan oleh user cth(menklik mouse,keyboard,dll) atau dilakukan secara otomatis oleh API
 
-//addEventListener
-const p3 = document.querySelector('.p3');
-
-function ubahWarna () {
-    p3.style.background
-}
-p3.onClick = ubahwarna;
+//addEventListener('string',listener)s
+const p4 = document.querySelector('section#b p');
+p4.addEventListener('click',function() {
+    //membuat elemen baru di li
+    const ul = document.querySelector('section#b ul'); //tangkap parent li yaitu ul
+    const li = document.createElement('li');
+    const itemBaru = document.createTextNode('item baru')
+    li.appendChild(itemBaru);
+    ul.appendChild(li);
+});
