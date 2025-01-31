f859ea1896fe8a0b07d08cd13fbc575c515dd785
Guntyoo <aprastiyo065@gmail.com>
2025-01-31 21:35:59 +0700

    menyelesaikan latihan kedua bermain dengan warna

--- Javascript lanjutan/Dom manipulation/latihanwarna.js
+++ Javascript lanjutan/Dom manipulation/latihanwarna.js
@@ -3,25 +3,25 @@ const body = document.body; //tangkap element yang ingin diubah
 
 //membuat event ketika button diclick merubah warna menggunakan event handler
 Button.onclick = function () {
-    body.classList.toggle('biru-muda');  //penjelasan toggle dimanipmethod2
-}
+  body.classList.toggle("biru-muda"); //penjelasan toggle dimanipmethod2
+};
 
 //membuat button baru di JS
 
-const ButtonBaru = document.createElement('button'); //create element
-const isi = document.createTextNode('warna acak?'); //crate text
+const ButtonBaru = document.createElement("button"); //create element
+const isi = document.createTextNode("warna acak?"); //crate text
 ButtonBaru.appendChild(isi); //masukan teks ke button baru
 // body.appendChild(ButtonBaru); //button baru dimasukan ke body
-Button.setAttribute('type', 'button'); //mengatur type button
+Button.setAttribute("type", "button"); //mengatur type button
 Button.after(ButtonBaru); //button baru dimasukan setelah button yang lama
 
 //membuat fyungsi ketika button baru di click akan merubah warna background secara random
 
-ButtonBaru.addEventListener('click', function () {
-    const r = Math.round(Math.random() * 255 + 1);
-    const g = Math.round(Math.random() * 255 + 1);
-    const b = Math.round(Math.random() * 255 + 1);
-    body.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+ButtonBaru.addEventListener("click", function () {
+  const r = Math.round(Math.random() * 255 + 1);
+  const g = Math.round(Math.random() * 255 + 1);
+  const b = Math.round(Math.random() * 255 + 1);
+  body.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
 });
 //math round digunakan untuk membulatkan angka
 //math random digunakan untuk mengacak angka
@@ -29,4 +29,25 @@ ButtonBaru.addEventListener('click', function () {
 //' + r + ' adalah cara untuk menggabungkan string dengan variabel yang disebut operasi concatenation untuk
 //menggabungkan string dengan variabel agar bisa dijadikan satu string
 
+//membuat fungsi Kotak merah
+const Merah = document.querySelector("input[name=sMerah]"); //cari input yang dia punya name sMerah
+Merah.addEventListener("input", function () {
+    const r = Merah.value;
+        document.body.style.backgroundColor = "rgb(" + r + ",100,100)";
+});
+const Hijau = document.querySelector("input[name=sHijau]");
+Hijau.addEventListener("input", function () {
+    const r = Merah.value;
+        const g = Hijau.value;
+            document.body.style.backgroundColor = "rgb(" + r + "," + g + ",100)";
+});
+const Biru = document.querySelector("input[name=sBiru]");
+Biru.addEventListener("input", function () {
+    const r = Merah.value;
+        const g = Hijau.value;
+            const b = Biru.value;
+  document.body.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+});
 
+//input adalah sebuah event yang digunakan untuk mengambil inputan dari user
+// value digunakn untuk mengambil nilai dari object yang telah dibuat dan di input dari user
