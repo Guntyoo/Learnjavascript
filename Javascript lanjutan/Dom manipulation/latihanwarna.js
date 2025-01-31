5eda0bb640785fbdf7255c9e3fa38e647134b08c
Guntyoo <aprastiyo065@gmail.com>
2025-01-31 20:06:05 +0700

    membuat button baru dan fungsi untuk mengacak warna

--- Javascript lanjutan/Dom manipulation/latihanwarna.js
+++ Javascript lanjutan/Dom manipulation/latihanwarna.js
@@ -9,4 +9,24 @@ Button.onclick = function () {
 //membuat button baru di JS
 
 const ButtonBaru = document.createElement('button'); //create element
-const isi = document.createTextNode('mau warna yang berbeda?');
+const isi = document.createTextNode('warna acak?'); //crate text
+ButtonBaru.appendChild(isi); //masukan teks ke button baru
+// body.appendChild(ButtonBaru); //button baru dimasukan ke body
+Button.setAttribute('type', 'button'); //mengatur type button
+Button.after(ButtonBaru); //button baru dimasukan setelah button yang lama
+
+//membuat fyungsi ketika button baru di click akan merubah warna background secara random
+
+ButtonBaru.addEventListener('click', function () {
+    const r = Math.round(Math.random() * 255 + 1);
+    const g = Math.round(Math.random() * 255 + 1);
+    const b = Math.round(Math.random() * 255 + 1);
+    body.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+});
+//math round digunakan untuk membulatkan angka
+//math random digunakan untuk mengacak angka
+// * 255 + 1 adalah sebuah rumus yg digunakan untuk mengacak angka dari 1 - 255
+//' + r + ' adalah cara untuk menggabungkan string dengan variabel yang disebut operasi concatenation untuk
+//menggabungkan string dengan variabel agar bisa dijadikan satu string
+
+
