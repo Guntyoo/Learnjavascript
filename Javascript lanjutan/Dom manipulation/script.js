67151bb9fc2c3726e7155a52a92b7c0c164b2b70
Guntyoo <aprastiyo065@gmail.com>
2025-02-10 20:39:16 +0700

    suwit jawa 1

---
+++ Javascript lanjutan/Dom manipulation/script.js
@@ -0,0 +1,22 @@
+//comp
+function LogicComp () {
+    const comp = Math.random();
+
+    if (comp < 0.34) return "gajah";
+    if (comp >= 0.34 && comp < 0.67) return  "orang";
+    return  "semut";
+};
+
+//getresult
+function LogicHasil (comp,player) {
+    if (player == comp) return "SERI!";
+    if (player == "gajah") return (comp == "orang") ? "MENANG!" : "KALAH!";
+    if (player == "orang")return (comp == "gajah") ? "KALAH!" : "MENANG!";
+    if (player == "semut")return (comp == "orang") ? "KALAH!" : "MENANG!";
+};
+
+//getplayer
+const GPlayer = document.querySelector('.gajah');
+    GPlayer.addEventListener('click', function () {
+            alert('test');
+        });
