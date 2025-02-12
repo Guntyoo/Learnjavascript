eebaabb33924f2a0dece8e9e00b09e6e8816c470
Guntyoo <aprastiyo065@gmail.com>
2025-02-12 21:13:10 +0700

    succes made getplayer&comp

--- Javascript lanjutan/Dom manipulation/script.js
+++ Javascript lanjutan/Dom manipulation/script.js
@@ -1,5 +1,5 @@
 //comp
-function LogicComp () {
+function getLogicComp () {
     const comp = Math.random();
 
     if (comp < 0.34) return "gajah";
@@ -7,7 +7,7 @@ function LogicComp () {
     return  "semut";
 };
 
-//getresult
+//rules
 function LogicHasil (comp,player) {
     if (player == comp) return "SERI!";
     if (player == "gajah") return (comp == "orang") ? "MENANG!" : "KALAH!";
@@ -15,16 +15,25 @@ function LogicHasil (comp,player) {
     if (player == "semut")return (comp == "orang") ? "KALAH!" : "MENANG!";
 };
 
-//getplayer
+//getplayer&comp
     const GPlayer = document.querySelector('.gajah');
-    GPlayer.addEventListener('click', function () {
-            alert('test');
+        GPlayer.addEventListener('click', function () {
+            const getpilihancomp = getLogicComp();
+            const getpilihanplayer = GPlayer.className;
+            console.log(getpilihancomp);
+            console.log(getpilihanplayer);
         });
     const OPlayer = document.querySelector(".orang");
-    OPlayer.addEventListener("click", function () {
-      alert("test");
+        OPlayer.addEventListener("click", function () {
+            const getpilihancomp = getLogicComp();
+            const getpilihanplayer = OPlayer.className;
+            console.log(getpilihancomp);
+            console.log(getpilihanplayer);
     });
     const SPlayer = document.querySelector(".semut");
-    SPlayer.addEventListener("click", function () {
-      alert("test");
+        SPlayer.addEventListener("click", function () {
+            const getpilihancomp = getLogicComp();
+            const getpilihanplayer = SPlayer.className;
+            console.log(getpilihancomp);
+            console.log(getpilihanplayer);
     });
