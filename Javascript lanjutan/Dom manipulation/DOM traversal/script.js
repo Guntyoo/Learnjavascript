9e88c2bdb36996e606a9cc074de4d5dfd8ec0df2
Guntyoo <aprastiyo065@gmail.com>
2025-02-21 19:54:20 +0700

    parentElement and target

--- Javascript lanjutan/Dom manipulation/DOM traversal/script.js
+++ Javascript lanjutan/Dom manipulation/DOM traversal/script.js
@@ -1,6 +1,19 @@
-//remove card
-const card = document.querySelector('.card');
-const close = document.querySelector('.close');
-    close.addEventListener('click', function () {
-        card.remove(close);
-    } )
+ //remove card
+// const card = document.querySelector('.card');
+// const close = document.querySelector('.close');
+//     close.addEventListener('click', function () {
+//         card.remove(close);
+//     });
+
+
+//remove all card
+const close = document.querySelectorAll('.close');
+for (let i = 0; close.length; i++) {
+    close[i].addEventListener('click', function (e) {
+        // close[i].parentElement.remove();
+        e.target.parentElement.style.display = 'none';   //keduanya memiliki fungsi yang sama
+    });
+}
+
+//parantElement menampilkan parent dari element yang di cari
+//e.target menampilkan objek yang di klik
