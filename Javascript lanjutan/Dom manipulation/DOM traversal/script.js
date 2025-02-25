bdf1227d40ee27b1d4b7959f69a71a2d3da0c35d
Guntyoo <aprastiyo065@gmail.com>
2025-02-25 20:25:54 +0700

    rest

--- Javascript lanjutan/Dom manipulation/DOM traversal/script.js
+++ Javascript lanjutan/Dom manipulation/DOM traversal/script.js
@@ -8,12 +8,25 @@
 
 //remove all card
 const close = document.querySelectorAll('.close');
-for (let i = 0; close.length; i++) {
-    close[i].addEventListener('click', function (e) {
-        // close[i].parentElement.remove();
-        e.target.parentElement.style.display = 'none';   //keduanya memiliki fungsi yang sama
-    });
-}
+// for (let i = 0; close.length; i++) {
+//     close[i].addEventListener('click', function (e) {
+//         // close[i].parentElement.remove();
+//         e.target.parentElement.style.display = 'none';   //keduanya memiliki fungsi yang sama
+//     });
+// }
+
+close.forEach(function (el) {
+    el.addEventListener('click',function (e) {
+        e.target.parentElement.style.display = 'none';
+    })
+})
 
 //parantElement menampilkan parent dari element yang di cari
 //e.target menampilkan objek yang di klik
+
+//DOm traversal method (lihat gambar dom method di img!)
+const nama = document.querySelector('.nama');
+console.log(nama.parentElement);  //menampilkan parent dari element yang di cari
+console.log(nama.parentNode);  //menampilkan parent dari NODE yang di cari
+console.log(nama.nextElementSibling); //menampilkan element selanjutnya
+//rest
