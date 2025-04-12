6b803f5499f6772e75d5aec29fa9bacd4581b927
Guntyoo <aprastiyo065@gmail.com>
2025-04-12 23:32:00 +0700

    try to back

---
+++ Javascript lanjutan/Dom manipulation/Image gallery/script.js
@@ -0,0 +1,11 @@
+
+const container = document.querySelector('.container');
+const jumbo = document.querySelector('.jumbo');
+    container.addEventListener('click',function (e) {
+        
+        //cek apakah yang di klik adalah thumb
+        if (e.target.className == 'thumb') {
+            jumbo.src = e.target.src;
+            jumbo.classList.add('.fade');
+        }
+    })
